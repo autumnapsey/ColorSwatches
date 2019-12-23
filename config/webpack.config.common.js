8ee306123078f636862fb33513b058f2d455fd9a
Autumn Apsey <autumnapsey@gmail.com>
2019-12-23 03:41:28 -0500

    Prepare for deployment and small wip fix for small screen viewing

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../build'),
+    path: path.resolve(__dirname, '../docs'),
     publicPath: '/',
   },
   module: {
